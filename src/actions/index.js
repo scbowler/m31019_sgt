b827f808cdc74dd7e598fdf8a23cd085cb632786
Scott Bowler <scott.bowler@learningfuze.com>
2019-11-30 10:54:17 -0800

    Setup thunk middleware and got grade data from server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,25 @@
 import types from './types';
 import axios from 'axios';
+
+export function getGradeData(){
+    return async function (dispatch) {
+        const resp = await axios.get('/api/grades');
+
+        dispatch({
+            type: types.GET_GRADE_DATA,
+            grades: resp.data
+        });
+    }
+}
+
+
+// export async function getGradeData() { 
+//     const resp = await axios.get('/api/grades');
+
+//     console.log('Get Data Response:', resp);
+
+//     return {
+//         type: 'GET_GRADE_DATA',
+//         data: resp.data
+//     }
+// }
